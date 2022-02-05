d0b9e72f47c5b8449b18673c1ceae8d97c718d02
siren15 <slowsc2@gmail.com>
2022-02-05 19:46:14 +0100

    better permissions check

--- extentions/giveroles.py
+++ extentions/giveroles.py
@@ -1,6 +1,6 @@
 import re
 
-from dis_snek import Snake, slash_command, InteractionContext, OptionTypes, Permissions, Scale, Embed
+from dis_snek import Snake, slash_command, InteractionContext, OptionTypes, Permissions, Scale, Embed, check
 from .src.mongo import *
 from .src.slash_options import *
 from .src.customchecks import *
@@ -12,135 +12,131 @@ class GiveRoles(Scale):
     @slash_command(name='giveyou', sub_cmd_name='_', sub_cmd_description="Give members a role from predefined list of roles")
     @user()
     @giveyou_name()
+    @check(member_permissions(Permissions.MANAGE_ROLES))
     async def giveyourole(self, ctx: InteractionContext, user:OptionTypes.USER=None, giveyou_name:str=None):
+        if user == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a user"), ephemeral=True)
+        if giveyou_name == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
+
         await ctx.defer()
-        perms = await has_perms(ctx.author, Permissions.MANAGE_ROLES)
-        if (perms == True):
-            if user == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a user"), ephemeral=True)
-            if giveyou_name == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
+        db = await odm.connect()
+        gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
 
-            db = await odm.connect()
-            gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
+        if gy == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: Couldn't find `{giveyou_name}` as a giveyou for {ctx.guild.name}"), ephemeral=True)
 
-            if gy == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: Couldn't find `{giveyou_name}` as a giveyou for {ctx.guild.name}"), ephemeral=True)
+        role = await ctx.guild.get_role(gy.roleid)
 
-            role = await ctx.guild.get_role(gy.roleid)
-
-            if role in user.roles:
-                await user.remove_role(role, reason=f'{ctx.author.display_name}|{ctx.author.id} removed a giveyou {giveyou_name}')
-                embed = Embed(
-                    description=f"I have removed {role.mention} from {user.mention}",
-                    color=0x0c73d3)
-            elif role not in user.roles:
-                await user.add_role(role, reason=f'{ctx.author.display_name}|{ctx.author.id} assigned a giveyou {giveyou_name}')
-                embed = Embed(
-                    description=f"I have assigned {role.mention} to {user.mention}",
-                    color=0x0c73d3)
-            await ctx.send(embed=embed)
+        if role in user.roles:
+            await user.remove_role(role, reason=f'{ctx.author.display_name}|{ctx.author.id} removed a giveyou {giveyou_name}')
+            embed = Embed(
+                description=f"I have removed {role.mention} from {user.mention}",
+                color=0x0c73d3)
+        elif role not in user.roles:
+            await user.add_role(role, reason=f'{ctx.author.display_name}|{ctx.author.id} assigned a giveyou {giveyou_name}')
+            embed = Embed(
+                description=f"I have assigned {role.mention} to {user.mention}",
+                color=0x0c73d3)
+        await ctx.send(embed=embed)
     
     @slash_command(name='giveyou', sub_cmd_name='create', sub_cmd_description="Create giveyou's")
     @giveyou_name()
     @role()
+    @check(member_permissions(Permissions.ADMINISTRATOR))
     async def giveyourole_create(self, ctx: InteractionContext, giveyou_name:str=None, role:OptionTypes.ROLE=None):
+        if giveyou_name == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
+        if role == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a role"), ephemeral=True)
+
         await ctx.defer()
-        perms = await has_perms(ctx.author, Permissions.ADMINISTRATOR)
-        if (perms == True):
-            if giveyou_name == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
-            if role == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a role"), ephemeral=True)
+        db = await odm.connect()
+        gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
 
-            db = await odm.connect()
-            gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
+        if gy != None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: `{giveyou_name}` already exists as a giveyou for {ctx.guild.name}"), ephemeral=True)
 
-            if gy != None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: `{giveyou_name}` already exists as a giveyou for {ctx.guild.name}"), ephemeral=True)
-
-            await db.save(giveyou(guildid=ctx.guild_id, name=giveyou_name, roleid=role.id))
-            await ctx.send(embed=Embed(color=0x0c73d3, description=f"giveyou `{giveyou_name}` created for {role.mention}"))
+        await db.save(giveyou(guildid=ctx.guild_id, name=giveyou_name, roleid=role.id))
+        await ctx.send(embed=Embed(color=0x0c73d3, description=f"giveyou `{giveyou_name}` created for {role.mention}"))
     
     @slash_command(name='giveyou', sub_cmd_name='delete', sub_cmd_description="Delete giveyou's")
     @giveyou_name()
+    @check(member_permissions(Permissions.ADMINISTRATOR))
     async def giveyourole_delete(self, ctx: InteractionContext, giveyou_name:str=None):
+        if giveyou_name == None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
+
         await ctx.defer()
-        perms = await has_perms(ctx.author, Permissions.ADMINISTRATOR)
-        if (perms == True):
-            if giveyou_name == None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=":x: Please provide a giveyou name"), ephemeral=True)
+        db = await odm.connect()
+        gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
 
-            db = await odm.connect()
-            gy = await db.find_one(giveyou, {'name':re.compile(f"^{giveyou_name}$", re.IGNORECASE), 'guildid': ctx.guild_id})
+        if gy != None:
+            return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: `{giveyou_name}` is not a giveyou for {ctx.guild.name}"), ephemeral=True)
 
-            if gy != None:
-                return await ctx.send(embed=Embed(color=0xDD2222, description=f":x: `{giveyou_name}` is not a giveyou for {ctx.guild.name}"), ephemeral=True)
-
-            role = await ctx.guild.get_role(gy.roleid)
-            await ctx.send(embed=Embed(color=0x0c73d3, description=f"giveyou `{giveyou_name}` deleted from {role.mention}"))
-            await db.delete(gy)
+        role = await ctx.guild.get_role(gy.roleid)
+        await ctx.send(embed=Embed(color=0x0c73d3, description=f"giveyou `{giveyou_name}` deleted from {role.mention}"))
+        await db.delete(gy)
     
     @slash_command(name='giveyou', sub_cmd_name='list', sub_cmd_description="Lists all giveyous for guild")
+    @check(member_permissions(Permissions.MANAGE_ROLES))
     async def giveyourole_list(self, ctx: InteractionContext):
         await ctx.defer()
-        perms = await has_perms(ctx.author, Permissions.MANAGE_ROLES)
-        if (perms == True):
-            from dis_snek.ext.paginators import Paginator
-            def chunks(l, n):
-                n = max(1, n)
-                return (l[i:i+n] for i in range(0, len(l), n))
-            
-            def mlis(lst, s, e):
-                nc = list(chunks(lst, 20))
-                mc = ''
-                for l in nc[s:e]:
-                    for m in l:
-                        mc = mc + m
-                return mc
+        from .src.paginators import Paginator
+        def chunks(l, n):
+            n = max(1, n)
+            return (l[i:i+n] for i in range(0, len(l), n))
+        
+        def mlis(lst, s, e):
+            nc = list(chunks(lst, 20))
+            mc = ''
+            for l in nc[s:e]:
+                for m in l:
+                    mc = mc + m
+            return mc
 
-            def newpage(title, names, roles):
-                embed = Embed(title=title,
-                color=0x0c73d3)
-                embed.add_field(name='Giveyou', value=names, inline=True)
-                embed.add_field(name='Role', value=roles, inline=True)
-                return embed
+        def newpage(title, names, roles):
+            embed = Embed(title=title,
+            color=0x0c73d3)
+            embed.add_field(name='Giveyou', value=names, inline=True)
+            embed.add_field(name='Role', value=roles, inline=True)
+            return embed
 
-            db = await odm.connect()
-            gy = await db.find(giveyou, {"guildid":ctx.guild_id})
-            
-            names = [str(g.name)+"\n" for g in gy]
-            if names == []:
-                embed = Embed(description=f"There are no giveyous for {ctx.guild.name}.",
-                            color=0x0c73d3)
-                await ctx.send(embed=embed)
-                return
-            roles = []
-            for g in gy:
-                role = await ctx.guild.get_role(g.roleid)
-                if role == None:
-                    roles.append('[ROLE NOT FOUND]\n')
-                else:
-                    roles.append(f"{role.mention}\n")
+        db = await odm.connect()
+        gy = await db.find(giveyou, {"guildid":ctx.guild_id})
+        
+        names = [str(g.name)+"\n" for g in gy]
+        if names == []:
+            embed = Embed(description=f"There are no giveyous for {ctx.guild.name}.",
+                        color=0x0c73d3)
+            await ctx.send(embed=embed)
+            return
+        roles = []
+        for g in gy:
+            role = await ctx.guild.get_role(g.roleid)
+            if role == None:
+                roles.append('[ROLE NOT FOUND]\n')
+            else:
+                roles.append(f"{role.mention}\n")
 
-            s = -1
-            e = 0
-            embedcount = 1
-            nc = list(chunks(names, 20))
+        s = -1
+        e = 0
+        embedcount = 1
+        nc = list(chunks(names, 20))
+        
+        embeds = []
+        while embedcount <= len(nc):
+            s = s+1
+            e = e+1
+            embeds.append(newpage(f'List of giveyous for {ctx.guild.name}', mlis(names, s, e), mlis(roles, s, e)))
+            embedcount = embedcount+1
             
-            embeds = []
-            while embedcount <= len(nc):
-                s = s+1
-                e = e+1
-                embeds.append(newpage(f'List of giveyous for {ctx.guild.name}', mlis(names, s, e), mlis(roles, s, e)))
-                embedcount = embedcount+1
-                
-            paginator = Paginator(
-                client=self.bot, 
-                pages=embeds,
-                timeout_interval=80,
-                show_select_menu=False)
-            await paginator.send(ctx)
+        paginator = Paginator(
+            client=self.bot, 
+            pages=embeds,
+            timeout_interval=80,
+            show_select_menu=False)
+        await paginator.send(ctx)
     
     # @slash_command(name='colourme', description="Give yourself a colour role from predefined list of roles. You are able to have just one.")
     # @colourme_name()
@@ -330,7 +326,7 @@ class GiveRoles(Scale):
     # @slash_command(name='colorme', sub_cmd_name='list', sub_cmd_description="Lists all colourmes for guild")
     # async def colourme_list(self, ctx: InteractionContext):
     #     await ctx.defer()
-    #     from dis_snek.ext.paginators import Paginator
+    #     from .src.paginators import Paginator
     #     def chunks(l, n):
     #         n = max(1, n)
     #         return (l[i:i+n] for i in range(0, len(l), n))
