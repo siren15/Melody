a8de1e62853e5d1a4c903ce1ffadc5a1792aae94
siren15 <slowsc2@gmail.com>
2022-02-14 21:08:57 +0100

    tasks import fix

--- extentions/moderation.py
+++ extentions/moderation.py
@@ -764,8 +764,8 @@ class Moderation(Scale):
                     embed.set_footer(text=f'User ID: {message.author.id}')
                     await log_channel.send(embed=embed)
 
-    from dis_snek.ext.tasks.task import Task
-    from dis_snek.ext.tasks.triggers import IntervalTrigger
+    from dis_snek.models.snek.tasks import Task
+    from dis_snek.models.snek.tasks.triggers import IntervalTrigger
 
     @Task.create(IntervalTrigger(seconds=60))
     async def unban_task(self):
