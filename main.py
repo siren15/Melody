9f0666d0933882178891e9eba4d7da6879f2be9e
siren15 <slowsc2@gmail.com>
2022-02-16 23:03:50 +0100

    auto deferring

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 import asyncio
 
-from dis_snek import Snake, Intents, listen, Embed, InteractionContext
+from dis_snek import Snake, Intents, listen, Embed, InteractionContext, AutoDefer
 from extentions.src.customchecks import *
+from extentions.src.mongo import *
 from dis_snek.client.errors import NotFound
+
+# import logging
+# import dis_snek
+# logging.basicConfig()
+# cls_log = logging.getLogger(dis_snek.const.logger_name)
+# cls_log.setLevel(logging.DEBUG)
 
 intents = Intents.ALL
 
@@ -13,7 +20,7 @@ class CustomSnake(Snake):
             embed = Embed(description=f":x: {ctx.author.mention} You don't have permissions to perform that action",
                           color=0xdd2e44)
             await ctx.send(embed=embed, ephemeral=True)
-        
+
         elif isinstance(error, MissingRole):
             db = await odm.connect()
             regx = re.compile(f"^{ctx.invoked_name}$", re.IGNORECASE)
@@ -23,7 +30,7 @@ class CustomSnake(Snake):
                 embed = Embed(description=f":x: {ctx.author.mention} You don't have role {role.mention} that's required to use this command.",
                               color=0xDD2222)
                 await ctx.send(embed=embed, ephemeral=True)
-        
+
         elif isinstance(error, RoleNotFound):
             embed = Embed(description=f":x: Couldn't find that role",
                           color=0xDD2222)
@@ -49,7 +56,7 @@ class CustomSnake(Snake):
             embed = Embed(description=f":x: Command is not activated in the server.",
                           color=0xDD2222)
             await ctx.send(embed=embed, ephemeral=True)
-        
+
         elif isinstance(error, UserInBlacklist):
             embed = Embed(description=f":x: {ctx.author.mention} You are not allowed to use this command",
                           color=0xDD2222)
@@ -58,8 +65,15 @@ class CustomSnake(Snake):
             embed = Embed(description=f":x: An error occured while trying to execute `{ctx.invoked_name}` command: ```{error}```",
                           color=0xDD2222)
             await ctx.send(embed=embed, ephemeral=True)
-
-bot = CustomSnake(intents=intents, sync_interactions=True, delete_unused_application_cmds=True, default_prefix='p.', fetch_members=True)
+ad = AutoDefer(enabled=True, time_until_defer=0)
+bot = CustomSnake(intents=intents, 
+                  sync_interactions=True, 
+                  delete_unused_application_cmds=True, 
+                  default_prefix='p.', 
+                  fetch_members=True, 
+                  auto_defer=ad,
+                # asyncio_debug=True,
+                  )
 
 @listen()
 async def on_ready():
@@ -73,6 +87,7 @@ async def on_guild_join(event):
     db = await odm.connect()
     checkserver = await db.find_one(prefixes, {'guildid':event.guild.id})
     if checkserver == None:
+        #add server to database
         await db.save(prefixes(guildid=event.guild.id, prefix='p.'))
         guild = await bot.get_guild(435038183231848449)
         channel = guild.get_channel(932661537729024132)
@@ -80,7 +95,6 @@ async def on_guild_join(event):
 
 for filename in os.listdir('./extentions'):
     if filename.endswith('.py'):
-        if (filename != 'mongo.py') and (filename != 'slash_options.py'):
-            bot.load_extension(f'extentions.{filename[:-3]}')
+        bot.load_extension(f'extentions.{filename[:-3]}')
 
 bot.start(os.environ['pinetree_token'])
