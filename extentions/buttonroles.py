51474cd6209e430953ae53958564fa6b4b82346d
siren15 <slowsc2@gmail.com>
2022-03-13 18:37:00 +0100

    add button list cmd

--- extentions/buttonroles.py
+++ extentions/buttonroles.py
@@ -1,3 +1,4 @@
+from email import message
 import random
 from dis_snek import Snake, slash_command, InteractionContext, OptionTypes, Permissions, Scale, Embed, check, listen, Button, ButtonStyles, spread_to_rows, SlashCommandChoice
 from dis_snek.models.discord.components import ActionRow, spread_to_rows
@@ -106,7 +107,7 @@ class ButtonRoles(Scale):
             await db.button_roles.insert_many(documents)
             await message.edit(components=rows)
             for m in messages:
-                await ctx.send(embed=Embed(color=0x0c73d3, descitpion=m))
+                await ctx.send(embed=Embed(color=0x0c73d3, descritpion=m))
     
     @slash_command(name='rolebuttons', sub_cmd_name='edit', sub_cmd_description="Edit a behaviour of a role button", scopes=[435038183231848449,149167686159564800])
     @message_id()
@@ -264,6 +265,87 @@ class ButtonRoles(Scale):
         button = await db.button_roles.find_one({'guildid':ctx.guild_id, 'channelid':channel.id, 'msg_id':message.id, 'button_id':button_id})
         await button.delete()
         await ctx.send(f"Button `{button_id}` successfully deleted")
+
+    @slash_command(name='rolebuttons', sub_cmd_name='list', sub_cmd_description="List all role buttons on this server", scopes=[435038183231848449,149167686159564800])
+    async def rb_list(self, ctx: InteractionContext):
+        from dis_snek.ext.paginators import Paginator
+        def chunks(l, n):
+            n = max(1, n)
+            return (l[i:i+n] for i in range(0, len(l), n))
+        
+        def mlis(lst, s, e):
+            nc = list(chunks(lst, 20))
+            mc = ''
+            for testlist in nc[s:e]:
+                for m in testlist:
+                    mc = mc + m
+            return mc
+
+        def newpage(title, buttoninfo):
+            embed = Embed(title=title,
+            description=buttoninfo,
+            color=0x0c73d3)
+            return embed
+        
+        role_buttons = db.button_roles.find({'guildid':ctx.guild_id})
+        
+        buttons = []
+        async for button in role_buttons:
+            role = ctx.guild.get_role(button.roleid)
+            if role is None:
+                role = 'Role missing'
+            elif role is not None:
+                role = role.mention
+            if button.requirement_role_id is not None:
+                req_role = ctx.guild.get_role(button.requirement_role_id)
+                if req_role is None:
+                    reqrole = 'Req. role missing'
+                elif role is not None:
+                    reqrole = req_role.mention
+            else:
+                reqrole = 'No req. role'
+            if button.ignored_role_id is not None:
+                ign_role = ctx.guild.get_role(button.ignored_role_id)
+                if ign_role is None:
+                    ignrole = 'Ign. role missing'
+                elif role is not None:
+                    ignrole = ign_role.mention
+            else:
+                ignrole = 'No ign. role'
+            channel = ctx.guild.get_channel(button.channelid)
+            if channel is None:
+                chnl = 'Channel missing'
+            elif channel is not None:
+                chnl = channel.mention
+            msg = await channel.fetch_message(button.msg_id)
+            if msg is None:
+                msg = 'Message missing'
+            elif msg is not None:
+                msg = f'[[Jump to message]]({msg.jump_url})'
+            buttons.append(f"**Button ID:** {button.button_id} | **Role:** {role} | **Req. role:** {reqrole} | **Ign. role:** {ignrole} |** Channel:** {chnl} | **Message:** {msg}\n\n")
+        
+        if buttons == []:
+            await ctx.send('There are no role buttons for this server yet')
+            return            
+        
+        s = -1
+        e = 0
+        embedcount = 1
+        nc = list(chunks(buttons, 20))
+        
+        embeds = []
+        while embedcount <= len(nc):
+            s = s+1
+            e = e+1
+            embeds.append(newpage(f'Role buttons for {ctx.guild.name}', mlis(buttons, s, e)))
+            embedcount = embedcount+1
+            
+        paginator = Paginator(
+            client=self.bot, 
+            pages=embeds,
+            timeout_interval=80,
+            show_select_menu=False)
+        await paginator.send(ctx)
 
     @listen()
     async def on_button_press_role_add_mode_1(self, event: Component):
