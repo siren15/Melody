7b64a2e5063f115c760249e110ff9e2eaf722aac
siren15 <slowsc2@gmail.com>
2022-09-01 17:44:23 +0200

    quick fix

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ def is_hex_valid(str):
     p = re.compile(regex)
     if(str == None):
         return False
-    if(re.search(p, re.escape(str))):
+    if(re.search(p, str)):
         return True
     else:
         return False
